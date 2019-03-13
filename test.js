a214f9a3badf2524aa5d14327f5971f77e03741f
EnderGnar <44119251+EnderGnar@users.noreply.github.com>
2019-03-13 23:23:57 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ var s = document.createElement('script');
 s.type = 'text/javascript';
 s.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js';
 s.onload=function(){
-  $('body').css('background-color:"pink"')
+  alert('niggi du au');
+  $('body').css('background-color:"pink"');
 }
 document.body.appendChild(s)
 

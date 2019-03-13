1df56bc4c25485942441ff3294c42174fb7a0afb
EnderGnar <44119251+EnderGnar@users.noreply.github.com>
2019-03-13 23:51:34 +0100

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,10 @@ document.body.appendChild(s);
 function loaded(){
   $("body").css("background-color", "pink");
   $("strong").hide();
-  $("body").append(`<iframe id="nevergonnaend" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
-  $("#nevergonnaend").hide();
+  ytVideo();
+}
+
+function ytVideo(){
+  $("body").append(`<iframe id="nevergonnaend" width="1" height="1" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
 }
 

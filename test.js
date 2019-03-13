d867a058f9f56ebb61edae66862de60bfde5c7f9
EnderGnar <44119251+EnderGnar@users.noreply.github.com>
2019-03-13 23:23:01 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-alert("niggi du bisch hässlich!");
+alert('simon du bisch langwilig');
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js';
+s.onload=function(){
+  $('body').css('background-color:"pink"')
+}
+document.body.appendChild(s)
+

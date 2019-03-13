2d00f43b47baf4cec00a03cec617b3db8710ee44
EnderGnar <44119251+EnderGnar@users.noreply.github.com>
2019-03-13 23:37:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-alert('simon du bisch langwilig');
 var s = document.createElement('script');
 s.type = 'text/javascript';
 s.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js';
-s.onload=function(){
-  alert('niggi du au');
+s.onload=loaded;
+document.body.appendChild(s);
+
+function loaded(){
   $("body").css("background-color", "pink");
+  $("strong").hide();
+  $("body").append(`<iframe id="nevergonnaend" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+  $("#nevergonnaend").hide();
 }
-document.body.appendChild(s)
 
